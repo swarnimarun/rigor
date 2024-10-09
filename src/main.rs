9b65eb4123b51f6e51af5c0c040986824d71ba07
Swarnim Arun <swarnimarun11@gmail.com>
2024-10-09 22:52:59 +0530

    refactor: cli & add logs to tell users about progress

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod rigor;
 mod snap;
 
 use core::panic;
-use std::{collections::HashMap, str::FromStr};
+use std::{collections::HashMap, path::PathBuf, str::FromStr};
 
 use clap::Parser;
 use reqwest::{
@@ -11,7 +11,7 @@ use reqwest::{
 };
 
 /// rigor is a simple application for quick and dirty
-/// snapshot testing for rest api, it uses the `*.rigor` files in your current directory
+/// snapshot testing for rest api, it uses the `.rigor` files
 /// to run simple rest api tests
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
@@ -68,8 +68,22 @@ impl Init {
 
 #[derive(Debug, Parser)]
 struct Run {
-    #[clap(default_value = "https://echo.free.beeceptor.com")]
+    /// This provides the endpoint url for RIGOR_ENDPOINT env variable
+    /// you can use this instead of manually setting the env variable
+    #[arg(short, long, env = "RIGOR_ENDPOINT")]
     url: String,
+    /// This forces the endpoint to be against echo server this forces you to update
+    /// the snapshots as well if you update the rigor file
+    #[clap(default_value = ".")]
+    #[arg(short, long)]
+    snapshot_dir: PathBuf,
+    /// Path to the rigor file to use for running the tests
+    #[arg(short, long)]
+    #[clap(default_value = "test.rigor")]
+    path: PathBuf,
+    /// Overwrite the value for the snapshot
+    #[arg(short, long)]
+    overwrite: bool,
 }
 impl Run {
     pub(crate) fn run(self) {
@@ -79,75 +93,106 @@ impl Run {
             .unwrap();
         let mut map = HashMap::from_iter(std::env::vars());
         let key = "RIGOR_ENDPOINT".to_string();
-        if !map.contains_key(&key) {
-            map.insert(key, self.url);
+        map.insert(key, self.url);
+
+        let p = String::from(
+            self.path
+                .file_name()
+                .unwrap()
+                .to_str()
+                .expect("non unicode file name"),
+        ) + ".snapshot";
+        let snapshot_path = self.snapshot_dir.join(&p);
+        if self.overwrite {
+            _ = std::fs::remove_file(&snapshot_path);
         }
 
-        let cwd = std::env::current_dir().expect("failed to get the current directory");
-        for e in cwd.read_dir().expect("failed to read the dir") {
-            let de = e.expect("failed to read the directory entry");
-            let Ok(ft) = de.file_type() else { continue };
-            if ft.is_file() && de.file_name().as_encoded_bytes().ends_with(b".rigor") {
-                let mut r: rigor::Rigor = serde_json::from_slice(
-                    &std::fs::read(de.path()).expect("failed to read a `*.rigor` file"),
-                )
-                .expect("failed to deserialize .rigor file");
-                r.ensure_env(&map);
-                let expected = std::fs::read_to_string(de.path().with_extension("snapshot")).ok();
-                //let snapshot: Option<snap::Snapshot> =
-                //    expected.and_then(|b| serde_json::from_str(&b).ok());
-                let mut snapshot = snap::Snapshot { outputs: vec![] };
-                // run the rigor file
-                for (_i, test) in r.tests.iter().enumerate() {
-                    let method = match test.method.as_str() {
-                        "POST" => reqwest::Method::POST,
-                        "GET" => reqwest::Method::GET,
-                        "DELETE" => reqwest::Method::DELETE,
-                        "PUT" => reqwest::Method::PUT,
-                        "PATCH" => reqwest::Method::PATCH,
-                        "HEAD" => reqwest::Method::HEAD,
-                        "TRACE" => reqwest::Method::TRACE,
-                        _ => {
-                            panic!("invalid method {}", test.method)
-                        }
-                    };
-                    let mut req = reqwest::Request::new(
-                        method,
-                        Url::from_str(&r.endpoint).expect("failed to build url from endpoint"),
-                    );
-                    if let Some(v) = &test.payload {
-                        _ = req.body_mut().insert(reqwest::Body::from(v.to_string()));
-                    }
-                    if let Some(h) = &test.headers {
-                        for (k, v) in h {
-                            _ = req.headers_mut().insert(
-                                HeaderName::from_bytes(k.as_bytes()).unwrap(),
-                                HeaderValue::from_bytes(v.as_bytes()).unwrap(),
-                            );
-                        }
-                    }
-                    let client = reqwest::Client::new();
-                    // run each request serially preferrably avoid running stuff in parrallel for
-                    // now!
-                    let Ok(v) = runtime.block_on(async { client.execute(req).await }) else {
-                        panic!("failed to make api request");
-                    };
-                    let status_code = v.status().as_u16();
-                    let mut body: serde_json::Value = runtime
-                        .block_on(v.json())
-                        .expect("failed to read the body from the response");
-                    rigor::skip_fields(&mut body, &test.skip_payload_fields);
-                    snapshot.outputs.push(snap::Output { status_code, body });
+        if !self.path.exists() {
+            panic!("path doesn't exists, invalid path: {}", self.path.display());
+        }
+
+        if !self.path.is_file() {
+            panic!("path not file, invalid path: {}", self.path.display());
+        }
+
+        let mut r: rigor::Rigor = serde_json::from_slice(
+            &std::fs::read(&self.path).expect("failed to read a `*.rigor` file"),
+        )
+        .expect("failed to deserialize .rigor file");
+        r.ensure_env(&map);
+
+        let expected = std::fs::read_to_string(&snapshot_path).ok();
+        let mut snapshot = snap::Snapshot { outputs: vec![] };
+        // run the rigor file
+        for (_i, test) in r.tests.iter().enumerate() {
+            let method_str = test.method.clone();
+            let endpoint = r.endpoint.clone() + &test.route;
+            println!("Testing [{method_str}] URL: {endpoint}");
+            let method = match method_str.as_str() {
+                "POST" => reqwest::Method::POST,
+                "GET" => reqwest::Method::GET,
+                "DELETE" => reqwest::Method::DELETE,
+                "PUT" => reqwest::Method::PUT,
+                "PATCH" => reqwest::Method::PATCH,
+                "HEAD" => reqwest::Method::HEAD,
+                "TRACE" => reqwest::Method::TRACE,
+                _ => {
+                    panic!("invalid method {}", test.method)
                 }
-                let src = serde_json::to_string_pretty(&snapshot)
-                    .expect("failed to serialize reqwest::Response");
-                if let Some(expected) = expected {
-                    pretty_assertions::assert_str_eq!(src, expected);
-                } else {
-                    std::fs::write(de.path().with_extension("snapshot"), src)
-                        .expect("failed to write");
+            };
+            let mut req = reqwest::Request::new(
+                method,
+                Url::from_str(&endpoint).expect("failed to build url from endpoint"),
+            );
+            let payload = if let Some(v) = &test.payload {
+                _ = req.body_mut().insert(reqwest::Body::from(v.to_string()));
+                Some(v)
+            } else {
+                None
+            };
+            if let Some(h) = &test.headers {
+                for (k, v) in h {
+                    _ = req.headers_mut().insert(
+                        HeaderName::from_bytes(k.as_bytes()).unwrap(),
+                        HeaderValue::from_bytes(v.as_bytes()).unwrap(),
+                    );
                 }
             }
+            let client = reqwest::Client::new();
+            // run each request serially preferrably avoid running stuff in parallel for now!
+            let v = match runtime.block_on(async { client.execute(req).await }) {
+                Err(err) => panic!("failed to make api request {err:#?}"),
+                Ok(v) => v,
+            };
+            let status_code = v.status().as_u16();
+            if !test
+                .expected_status_code
+                .map(|s| s == status_code)
+                .unwrap_or(true)
+            {
+                panic!("failed to match status_code in rigor file to response status_code");
+            }
+            let mut body: serde_json::Value = runtime
+                .block_on(v.json())
+                .expect("failed to read the body from the response");
+            rigor::skip_fields(&mut body, &test.skip_payload_fields);
+            snapshot.outputs.push(snap::Output {
+                endpoint,
+                method_str,
+                payload: payload.cloned(),
+                status_code,
+                body,
+            });
+        }
+        let src =
+            serde_json::to_string_pretty(&snapshot).expect("failed to serialize reqwest::Response");
+        if let Some(expected) = expected {
+            println!("Comparing results against the saved snapshot");
+            pretty_assertions::assert_str_eq!(src, expected);
+            println!("Matched successfully");
+        } else {
+            println!("Storing the new snapshot at: {}", snapshot_path.display());
+            std::fs::write(snapshot_path, src).expect("failed to write snapshot to path");
         }
     }
 }
--- src/snap.rs
+++ src/snap.rs
@@ -7,6 +7,10 @@ pub(crate) struct Snapshot {
 
 #[derive(Serialize, Deserialize)]
 pub(crate) struct Output {
+    pub method_str: String,
+    pub endpoint: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub payload: Option<serde_json::Value>,
     pub status_code: u16,
     pub body: serde_json::Value,
 }
